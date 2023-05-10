829fa0a01dc6f4899d0348fba1591d41ddec584d
Tomoya Ogawa <5stream418@gmail.com>
2023-05-10 13:25:52 +0900

    easy

---
+++ Arrays-Hashing/1.two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+                
+# @lc code=end
+
